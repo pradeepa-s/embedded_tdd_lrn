c988a506d9c0f94218791916dcb3464768a69936
Pradeepa Senanayake <pradeepas@zone24x7.com>
2015-11-24 19:40:05 +0530

    Made the code to pass the Test 2

--- tdd_learn/led_driver/src/led_driver.c
+++ tdd_learn/led_driver/src/led_driver.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "led_driver.h"
 
+static uint16_t *led_address;
+
 void LedDriver_Create(uint16_t *address)
 {
-	*address = 0;
+	led_address = address;
+	*led_address = 0;
 }
 
 void LedDriver_TurnOn(int led_no)
 {
+	*led_address = 1;
 }
