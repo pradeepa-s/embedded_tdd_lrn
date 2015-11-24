dba2ccfd42601982d96557238c64977a110d696c
Pradeepa Senanayake <pradeepas@zone24x7.com>
2015-11-24 19:28:14 +0530

    Added the led_driver.h file

---
+++ tdd_learn/led_driver/inc/led_driver.h
@@ -0,0 +1,8 @@
+#ifndef _LED_DRIVER_H
+#define _LED_DRIVER_H
+
+#include <stdint.h>
+
+void LedDriver_Create(uint16_t *address);
+
+#endif
--- tdd_learn/led_driver/tests/led_driver/led_driver_test.c
+++ tdd_learn/led_driver/tests/led_driver/led_driver_test.c
@@ -1,4 +1,5 @@
 #include "unity_fixture.h"
+#include "led_driver.h"
 
 TEST_GROUP(led_driver);
 
