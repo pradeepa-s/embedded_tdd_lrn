b265c730d3d0e30a4d943f6a472d6beb8fadef06
Pradeepa Senanayake <pradeepas@zone24x7.com>
2015-11-24 19:47:43 +0530

    Refactored the test

--- tdd_learn/led_driver/tests/led_driver/led_driver_test.c
+++ tdd_learn/led_driver/tests/led_driver/led_driver_test.c
@@ -3,9 +3,11 @@
 
 TEST_GROUP(led_driver);
 
+static uint16_t virtualLeds;
+
 TEST_SETUP(led_driver)
 {
-    
+	LedDriver_Create(&virtualLeds);    
 }
 
 TEST_TEAR_DOWN(led_driver)
@@ -21,16 +23,12 @@ TEST(led_driver, LedsOffAfterCreate)
 
 TEST(led_driver, TurnOnLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
 }
 
 TEST(led_driver, TurnOffLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
